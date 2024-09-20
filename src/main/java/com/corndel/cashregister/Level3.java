d29d18d033f983d0ce2d52b6ed734526d85317a3
Human0467 <mustchjo@gmail.com>
2024-09-20 14:05:08 +0100

    level 3 passing

--- src/main/java/com/corndel/cashregister/Level3.java
+++ src/main/java/com/corndel/cashregister/Level3.java
@@ -8,7 +8,11 @@ public class Level3 {
    * Calculates the total value of all money in the drawer.
    */
   public static int sumDrawer(List<Item> drawer) {
-    // TODO
-    return 0;
+    int totalInCents = 0;
+
+    for(Item item : drawer){
+      totalInCents += (item.getQuantity() * item.getValue());
+    }
+    return totalInCents;
   }
 }
